5a32bc9f98bd72efa20f81614696751b7f12181f
madhurikalani <35179732+madhurikalani@users.noreply.github.com>
2018-05-25 14:45:47 -0700

    Updating pkgs

--- Ad_Fraud_Detection/pkg/myfirstpackage/R/predict_glm.R
+++ Ad_Fraud_Detection/pkg/myfirstpackage/R/predict_glm.R
@@ -6,17 +6,38 @@
 #'
 #' @return model
 #'
-#' @examples  param_linear()
+#' @examples  predict_glm()
 #'
 #' @export predict_glm
 
 library(caret)
+library(data.table)
+library(lubridate)
 predict_glm <- function() {
-  readRDS(model_glm, refhook = NULL)
+  invisible(gc())
+  setwd("C:/Users/akash/Documents/Madhuri stuff/Data_Science_Intro_R/Ad_Fraud_Detection/pkg/myfirstpackage")
+  test <- fread("C:/Users/akash/Documents/Madhuri stuff/Data_Science_Intro_R/Ad_Fraud_Detection/test.csv", skip = 900000, nrows = 148576,
+                 col.names =c("click_id","ip", "app", "device", "os", "channel", "click_time"),
+                 showProgress = FALSE)
+  test$is_attributed <- 0
+  test$click_time <- as_datetime(test$click_time)
+  invisible(gc())
   print("Linear: Model Training")
-  model_logistic <- model_glm
-  predict_glm <- predict(model_logistic,df.test)
-  table(df.test$is_attributed,predict_glm)
-  print("Actual vs Predicted")
-  
+  model_logistic <- readRDS("model_glm.RDS")
+  summary(model_logistic)
+  predict <- predict(model_logistic,test,type="response")
+  #predict<-ifelse(predict > 0.5,1,0)
+  predict = as.data.frame(predict)
+  sub <- data.table(click_id = test$click_id, is_attributed = NA)
+  sub$is_attributed = predict
+  t <-getwd()
+  print(t)
+  fwrite(sub, "sub_glm.csv")
+
+  print("done")
+  invisible(gc())
+    print("Actual vs Predicted")
+    #return(table(test$is_attributed,predict))
+
 }
+
